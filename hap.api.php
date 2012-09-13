f5902f65bafae7bea6ba36798717a3e6373cf1be
Helior Colorado <me@helior.info>
2012-09-13 16:51:31 -0700

    Fixing whitespace.

--- hap.api.php
+++ hap.api.php
@@ -17,32 +17,32 @@ function hook_hap_info() {
   return array(
     // The Human-readable name for your tool.
     'name' => 'Crazy Horse',
-    
+
     // A brief description about what your tool does.
     'description' => t('Does unimaginable things to your page displays.'),
-    
+
     // Define the Drupal version this tool is compatible with. Defaults to the
     // current Drupal version.
     'core' => '7.x',
-    
+
     // A human-readable version label for wherever tools are usually displayed,
     // such as the "Attractive Installer" page. Defaults to the current Drupal and
     // HAP version.
     'version' => '7.x-3.1',
-    
+
     // The minimal php version your tool is compatible with. Defaults to
     // Drupal's current PHP minimum.
     'php' => '5.3',
-    
+
     // This determines whether a tool should ALWAYS be enabled and bypasses the
     // user option to enable/disable it in the "Attractive Installer". This is
     // only really applicable to the "core" tool that contains global
     // utilities.
     'always enabled' => FALSE,
-    
-    // Defines which hooks this module utilizes. 
+
+    // Defines which hooks this module utilizes.
     'hooks' => array('menu', 'theme', 'form_alter', 'page_alter'),
-    
+
     // List module/tool dependencies. This will only ensure that
     // enabling/disabling a tool is possible when the dependency is met. It
     // does not ensure the depended tool is automatically included.
