fab483d8f92312b21aa232de5c93011cc52ff7f9
GDrupal <GDrupal@1161294.no-reply.drupal.org>
2013-03-01 19:35:41 -0300

    Add Drupal behaviors to fix settings undefined bug.

--- js/timeago.js
+++ js/timeago.js
@@ -3,11 +3,15 @@
  */
 
 (function ($) {
-    if (typeof Drupal.settings.hap_timeago == "object") {
-        var current = new Date();
-        for(var nodeId in Drupal.settings.hap_timeago) {
-            date = new Date(Drupal.settings.hap_timeago[nodeId]*1000);
-            $("span.time-ago-" + nodeId).html(timeAgo(current,date).toString());
+    Drupal.behaviors.hap_timeago_render = {
+        attach: function (context, settings) {
+            if (typeof Drupal.settings.hap_timeago == "object") {
+                var current = new Date();
+                for(var nodeId in Drupal.settings.hap_timeago) {
+                    date = new Date(Drupal.settings.hap_timeago[nodeId]*1000);
+                    $("span.time-ago-" + nodeId).html(timeAgo(current,date).toString());
+                }
+            }
         }
     }
 })(jQuery);
@@ -24,26 +28,26 @@ function timeAgo(current, previous) {
     var elapsed = current - previous;
 
     if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + ' seconds agoX';
+         return Math.round(elapsed/1000) + ' seconds ago';
     }
 
     else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes agoX';
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';
     }
 
     else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + ' hours agoX';
+         return Math.round(elapsed/msPerHour ) + ' hours ago';
     }
 
     else if (elapsed < msPerMonth) {
-         return Math.round(elapsed/msPerDay) + ' days agoX';
+         return Math.round(elapsed/msPerDay) + ' days ago';
     }
 
     else if (elapsed < msPerYear) {
-         return Math.round(elapsed/msPerMonth) + ' months agoX';
+         return Math.round(elapsed/msPerMonth) + ' months ago';
     }
 
     else {
-         return Math.round(elapsed/msPerYear ) + ' years agoX';
+         return Math.round(elapsed/msPerYear ) + ' years ago';
     }
 }
