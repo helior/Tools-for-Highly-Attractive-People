5557c77d60855af4fe633f9ba45325ef07eda001
GDrupal <GDrupal@1161294.no-reply.drupal.org>
2013-03-01 19:49:04 -0300

    General format fixes.

--- js/timeago.js
+++ js/timeago.js
@@ -28,26 +28,26 @@ function timeAgo(current, previous) {
     var elapsed = current - previous;
 
     if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + ' seconds ago';
+        return Math.round(elapsed/1000) + ' seconds ago';
     }
 
     else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes ago';
+        return Math.round(elapsed/msPerMinute) + ' minutes ago';
     }
 
     else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + ' hours ago';
+        return Math.round(elapsed/msPerHour ) + ' hours ago';
     }
 
     else if (elapsed < msPerMonth) {
-         return Math.round(elapsed/msPerDay) + ' days ago';
+        return Math.round(elapsed/msPerDay) + ' days ago';
     }
 
     else if (elapsed < msPerYear) {
-         return Math.round(elapsed/msPerMonth) + ' months ago';
+        return Math.round(elapsed/msPerMonth) + ' months ago';
     }
 
     else {
-         return Math.round(elapsed/msPerYear ) + ' years ago';
+        return Math.round(elapsed/msPerYear ) + ' years ago';
     }
 }
